75bec245df52c8166e2c3fb83de03573458f9593
Lucas Mendes <lucas.d.mendes@ba.estudante.senai.br>
2025-06-27 11:50:25 -0300

    método login e constructor

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AutenticacaoService } from 'src/app/autenticacao/autenticacao.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  usuario = '';
+  senha = '';
+
+  constructor(private authService: AutenticacaoService) {}
+
+  ngOnInit(): void { }
+
+  login() {
+    this.authService.autenticar(this.usuario, this.senha).subscribe(() => {
+      console.log('Autenticado com sucesso!');
+    }, (error) => {
+      alert('Usuário ou senha inválido');
+      console.error();
+    });
+  }
 
 }
