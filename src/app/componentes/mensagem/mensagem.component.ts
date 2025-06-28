f8465712833e85d3548845d21b0c1497520ac131
Lucas Mendes <lucas.d.mendes@ba.estudante.senai.br>
2025-06-28 19:42:33 -0300

    mensagem e input

--- src/app/componentes/mensagem/mensagem.component.ts
+++ src/app/componentes/mensagem/mensagem.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-mensagem',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./mensagem.component.css']
 })
 export class MensagemComponent {
+  @Input()
+  mensagem = ''
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
